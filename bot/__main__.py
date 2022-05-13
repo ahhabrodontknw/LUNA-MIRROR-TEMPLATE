906eb3345b08f09398f866760aeb6e4ec1bdfca0
ahhabrodontknw <105362967+ahhabrodontknw@users.noreply.github.com>
2022-05-13 18:14:35 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,10 +45,10 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("ҒᏆᏞͲᎬᎡ ᏴϴͲ", "https://t.me/rb_luna_bot")
-    buttons.buildbutton("ᎷϴᏙᏆᎬ ᏀᎡϴႮᏢ", "https://t.me/Night_showers")
-    buttons.buildbutton("ᎷᏆᎡᎡϴᎡ ᏀᎡϴႮᏢ", "https://t.me/lunamirror")
-    buttons.buildbutton("ᎠᎬᏙᎬᏞϴᏢᎬᎡ", "https://t.me/balaSmurugan")
+    buttons.buildbutton("ҒᏆᏞͲᎬᎡ ᏴϴͲ", "not available")
+    buttons.buildbutton("ᎷϴᏙᏆᎬ ᏀᎡϴႮᏢ", "https://t.me/ssrrmirror")
+    buttons.buildbutton("ᎷᏆᎡᎡϴᎡ ᏀᎡϴႮᏢ", "https://t.me/ssrrmirror")
+    buttons.buildbutton("ᎠᎬᏙᎬᏞϴᏢᎬᎡ", "https://t.me/Yesbror")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
